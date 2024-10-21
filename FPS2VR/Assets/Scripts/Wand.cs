b309e2df3928a8cf0083e5b6a29391341c5e672d
cmonpalo <rapcrisram326@gmail.com>
2024-10-21 18:29:57 -0400

    Small Tweaks
    Added (Unnecessary) Interaction Manager, Changes to Vr perpsective, gogohand to interactable layer,

--- FPS2VR/Assets/Scripts/Wand.cs
+++ FPS2VR/Assets/Scripts/Wand.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Wand : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
